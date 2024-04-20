19938b1445c3176aaf07946facca2f8f1139bf57
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-20 16:40:27 +0900

    React - JSX 문법 활용하여 컴포넌트 만들기

---
+++ 주특기주차/React/first-react-project/src/components/Section.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Section = () => {
+    return (
+        <div>
+            <div>이건 div야</div>
+            <p>이건 p야</p>
+            <button>이건 버튼이야</button>
+        </div>
+    );
+};
+
+export default Section;
