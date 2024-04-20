143902fd76e59e4f2df4e22f8031d50d27368063
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-20 19:09:33 +0900

    React - toas message 만들기

--- 주특기주차/React/first-react-project/src/App.js
+++ 주특기주차/React/first-react-project/src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import TextComponent from './components/TextComponent';
+import Section from './components/Section';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <TextComponent/>
-      </header>
+    <div className="wrapper">
+      <div className="toast toast-success">
+        <main className="toast__message">
+          <header className="toast__message-title">Success</header>
+          <p className="toast__message-text">Right on!</p>
+        </main>
+        <button className="toast__button">Dismiss</button>
+      </div>
+      <div className="toast toast-warning">
+        <main className="toast__message">
+          <header className="toast__message-title">Warning</header>
+          <p className="toast__message-text">Hmmm!</p>
+        </main>
+        <button className="toast__button">Dismiss</button>
+      </div>
+      <div className="toast toast-error">
+        <main className="toast__message">
+          <header className="toast__message-title">Error</header>
+          <p className="toast__message-text">Oh!</p>
+        </main>
+        <button className="toast__button">Dismiss</button>
+      </div>
     </div>
   );
 }
