d1b7356728962f80f2cb376696fb1d1c83e947d6
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-20 21:23:13 +0900

    React - component에 props 넘겨주는 부분 코드를 map을 사용하도록 리팩토링

--- 주특기주차/React/first-react-project/src/App.js
+++ 주특기주차/React/first-react-project/src/App.js
@@ -1,32 +1,42 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import TextComponent from './components/TextComponent';
 import Section from './components/Section';
+import Toast from "./components/Toast";
 
 function App() {
+  const messageArray = [
+    {
+      title: 'success',
+      text: 'Right On!'
+    },
+    {
+      title: 'success',
+      text: 'Right On!'
+    },
+    {
+      title: 'warning',
+      text: 'Hmmm'
+    },
+    {
+      title: 'warning',
+      text: 'Hmmm'
+    },
+    {
+      title: 'error',
+      text: 'Uh Oh!'
+    },
+    {
+      title: 'error',
+      text: 'Uh Oh!'
+    }
+  ];
+
   return (
     <div className="wrapper">
-      <div className="toast toast-success">
-        <main className="toast__message">
-          <header className="toast__message-title">Success</header>
-          <p className="toast__message-text">Right on!</p>
-        </main>
-        <button className="toast__button">Dismiss</button>
-      </div>
-      <div className="toast toast-warning">
-        <main className="toast__message">
-          <header className="toast__message-title">Warning</header>
-          <p className="toast__message-text">Hmmm!</p>
-        </main>
-        <button className="toast__button">Dismiss</button>
-      </div>
-      <div className="toast toast-error">
-        <main className="toast__message">
-          <header className="toast__message-title">Error</header>
-          <p className="toast__message-text">Oh!</p>
-        </main>
-        <button className="toast__button">Dismiss</button>
-      </div>
+      {messageArray.map((message, index) =>  (
+          <Toast title={messageArray[index].title} text={messageArray[index].text}/>
+        ))}
     </div>
   );
 }
