90dafc38b71ae53ffe41fee9f11d4631ae650dc3
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-23 15:39:27 +0900

    React - 부모 컴포넌트의 상태를 자식 컴포넌트로 전달하기

--- 주특기주차/React/react-fundamental/src/App.js
+++ 주특기주차/React/react-fundamental/src/App.js
@@ -1,29 +1,38 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import PaymentForm from "./components/PaymentForm/PaymentForm";
 import Expenses from "./components/Payments/Expenses";
 
 function App() {
-
-	// 자식 컴포넌트에서 작성한 값을 부모 컴포넌트에서 찍어보기 위해 부모의 상태를 만들어준다
-	const [parentObjectState, setParentObjectState] = useState({
-		name: "",
-		price: 0,
-		today: new Date()
-	})
-
-	const expenses = [
+	const [expenses, setExpenses] = useState([
 		{
 			id: "e1",
 			title: "수건",
-			amount: 12.33,
-			date: new Date(2025, 8, 14),
+			amount: 3000,
+			date: new Date(2023, 11, 13),
 		},
-	];
+	]);
+
+	// 자식 컴포넌트에서 작성한 값을 부모 컴포넌트에서 찍어보기 위해 부모의 상태를 만들어준다
+	const [parentObjectState, setParentObjectState] = useState({
+		name: "수건",
+		price: 3000,
+		today: new Date(2023,11,13),
+	});
 
 	const getPaymentFormData = (data) => {
 		console.log(data);
-	}
+		// 부모 컴포넌트의 상태를 자식 컴포넌트에 전달
+		setExpenses([
+			{
+				id: Math.random().toString(),
+				title: data.name,
+				amount: data.price,
+				date: data.today,
+			},
+		]);
+
+	};
 
 	return (
 		<>
