700fb94314e7b4273eb735c6f38e440bd44012de
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-23 12:44:51 +0900

    React - 여러개의 상태 처리시 객체로 만들어서 처리하도록 코드 리팩토링

--- 주특기주차/React/react-fundamental/src/components/NewPayment/PaymentForm.js
+++ 주특기주차/React/react-fundamental/src/components/NewPayment/PaymentForm.js
@@ -3,29 +3,30 @@ import React, { useState } from 'react';
 import './PaymentForm.css';
 
 const PaymentForm = () => {
-
-  // state 만들기
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState(0);
-  const [today, setToday] = useState(null);
+  // 여러개의 state를 하나의 객체에 담아서 관리하기
+  const [objectState, setObjectState] = useState({
+    name: '',
+    price: 0,
+    today: null
+  })
 
   // onChange를 통해 input에서 값을 받고 이벤트를 감지한다
   const inputTypeTextChangeHandler = (event) => {
-      setName(event.target.value);   
+    setObjectState(prevState => ({...prevState, name: event.target.value}));   
   };
 
   const inputTypeNumberChangeHandler = (event) => {
-      setPrice(event.target.value);   
+    setObjectState(prevState => ({...prevState, price: event.target.value}));   
   };
 
   const inputTypeDateChangeHandler = (event) => {
-      setToday(event.target.value);   
+    setObjectState(prevState => ({...prevState, today: event.target.value}));   
   };
 
   const buttonSubmitHandler = () => {
-      console.log('name', name);
-      console.log('price', price);
-      console.log('today', today);
+      console.log('name', objectState.name);
+      console.log('price', objectState.price);
+      console.log('today', objectState.today);
   }
 
   return (
@@ -33,15 +34,15 @@ const PaymentForm = () => {
       <div className='new-payment__controls'>
         <div className='new-payment__control'>
           <label>이름</label>
-          <input type='text' value={name}  onChange={inputTypeTextChangeHandler}/>
+          <input type='text' value={objectState.name}  onChange={inputTypeTextChangeHandler}/>
         </div>
         <div className='new-payment__control'>
           <label>금액</label>
-          <input type='number' min='0.01' step='0.01' value={price}  onChange={inputTypeNumberChangeHandler}/>
+          <input type='number' min='0.01' step='0.01' value={objectState.price}  onChange={inputTypeNumberChangeHandler}/>
         </div>
         <div className='new-payment__control'>
           <label>날짜</label>
-          <input type='date' min='2024-01-01' max='2024-12-31' value={today} onChange={inputTypeDateChangeHandler}/>
+          <input type='date' min='2024-01-01' max='2024-12-31' value={objectState.today} onChange={inputTypeDateChangeHandler}/>
         </div>
       </div>
       <div className='new-payment__actions'>
