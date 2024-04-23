618cd6a93420c65a4782811008882e4ba1ab1581
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-23 15:03:52 +0900

    React - 자식 컴포넌트에서 부모 컴포넌트로 상태 올리기

--- 주특기주차/React/react-fundamental/src/App.js
+++ 주특기주차/React/react-fundamental/src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
-import NewPayment from './components/NewPayment/NewPayment';
+import "./App.css";
+import { useState } from "react";
+import PaymentForm from "./components/PaymentForm/PaymentForm";
+import Expenses from "./components/Payments/Expenses";
 
 function App() {
 
-  return (
-    <NewPayment/>
-  );
+	// 자식 컴포넌트에서 작성한 값을 부모 컴포넌트에서 찍어보기 위해 부모의 상태를 만들어준다
+	const [parentObjectState, setParentObjectState] = useState({
+		name: "",
+		price: 0,
+		today: new Date()
+	})
+
+	const expenses = [
+		{
+			id: "e1",
+			title: "수건",
+			amount: 12.33,
+			date: new Date(2025, 8, 14),
+		},
+	];
+
+	const getPaymentFormData = (data) => {
+		console.log(data);
+	}
+
+	return (
+		<>
+			<PaymentForm getPaymentFormData={getPaymentFormData} />
+			<Expenses items={expenses} />
+		</>
+	);
 }
 
 export default App;
