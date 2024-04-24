6ab826a1261af5d29ab86561a15c5338e374d000
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-24 13:26:41 +0900

    React - 컴포넌트 리스트에 필터링 걸기

---
+++ 주특기주차/React/react-drop-down-list-fundamental/src/components/Payments/Expenses.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from './ExpensesFilter';
+
+const Expenses = (props) => {
+	const [filteredYear, setFilteredYear] = useState('2023');
+
+	const filterChangeHandler = (selectedYear) => {
+		setFilteredYear(selectedYear);
+	};
+
+	// 아이템들을 필터 연도와 같은것만 먼저 보여준다
+	const filteredExpenses = props.items.filter((expenses) => {
+		return expenses.date.getFullYear().toString() === filteredYear;
+	});
+
+	let expensesContent = <p>값이 없습니다.</p>
+
+	if (filteredExpenses.length > 0) {
+		expensesContent = filteredExpenses.map((item) => (
+			<ExpenseItem
+				title={item.title}
+				amount={item.amount}
+				date={item.date}
+			/>
+		))
+	}
+	
+
+	return (
+		<Card className="expenses">
+			<ExpensesFilter
+				selected={filteredYear}
+				onChangeFilter={filterChangeHandler}
+			/>
+			{expensesContent}
+		</Card>
+	);
+};
+
+export default Expenses;
