b64e9e8e2c5700c060286e5b030827b6041a4d21
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-24 09:56:46 +0900

    React - filter를 활용하여 삭제기능 구현

--- 주특기주차/React/react-image-fundamental/src/App.js
+++ 주특기주차/React/react-image-fundamental/src/App.js
@@ -2,18 +2,70 @@ import AlexaImage from'./images/alexa.png'
 import SiriImage from'./images/siri.png'
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import PaymentForm from "./components/PaymentForm/PaymentForm";
+import Expenses from "./components/Payments/Expenses";
 
 console.log("AlexaImage", AlexaImage);
 console.log("SiriImage", SiriImage);
 
 function App() {
-  return (
+  const [expenses, setExpenses] = useState([
+		{
+			id: "e1",
+			title: "수건",
+			amount: 12.33,
+			date: new Date(2025, 8, 14),
+    },
+    {
+			id: "e2",
+			title: "빗자루",
+			amount: 120.33,
+			date: new Date(2025, 7, 14),
+    },
+    {
+			id: "e3",
+			title: "세탁기",
+			amount: 220.33,
+			date: new Date(2025, 9, 14),
+    },
+	]);
+
+	const getPaymentFormData = (data) => {
+    console.log('data', data);
+    console.log('expenses', expenses);
+		setExpenses([
+			{
+				id: Math.random().toString(),
+				title: data.name,
+				amount: data.price,
+				date: new Date(data.today),
+      },
+      ...expenses
+		]);
+	};
+
+  const deleteExpenseItem = (id) => {
+    console.log(id);
+    // 1. filter
+    const newFilteredArray = expenses.filter(item => item.id !== id);
+    setExpenses(newFilteredArray);
+
+  }
+
+
+
+	return (
+		<>
     <div className="App">
         <p>hello image</p>
         <img src={AlexaImage} alt="alexa"/>
         <img src={SiriImage} alt="siri"/>
     </div>
-  );
+			<PaymentForm getPaymentFormData={getPaymentFormData} />
+			<Expenses items={expenses} deleteExpenseItem={deleteExpenseItem}/>
+		</>
+	);
 }
 
 export default App;
