2a8a06185accb5a5a0dfcedb867c038363c009a7
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-04-25 11:29:33 +0900

    css-modules 적용을 위한 프로젝트 재세팅

--- 주특기주차/React/react-css-modules/src/App.js
+++ 주특기주차/React/react-css-modules/src/App.js
@@ -1,57 +1,59 @@
-import React, { useState } from 'react';
+import { useState } from "react";
+import "./App.css";
+import PaymentForm from "./components/PaymentForm/PaymentForm";
+import Expenses from "./components/Payments/Expenses";
 
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-import './App.css';
+function App() {
+	const [expenses, setExpenses] = useState([
+		{
+			id: "e1",
+			title: "수건",
+			amount: 12.33,
+			date: new Date(2022, 3, 14),
+		},
+		{
+			id: "e2",
+			title: "물티슈",
+			amount: 234.11,
+			date: new Date(2023, 8, 22),
+		},
+		{
+			id: "e3",
+			title: "모니터",
+			amount: 3331.33,
+			date: new Date(2021, 10, 22),
+		},
+		{
+			id: "e4",
+			title: "의자",
+			amount: 298,
+			date: new Date(2022, 1, 31),
+		},
+		{
+			id: "e4",
+			title: "의자",
+			amount: 298,
+			date: new Date(2021, 6, 1),
+		},
+	]);
 
-const App = () => {
-  const [courseGoals, setCourseGoals] = useState([
-    { text: '운동하기', id: 'g1' },
-    { text: '완강하기', id: 'g2' }
-  ]);
+	const getPaymentFormData = (data) => {
+		setExpenses([
+			{
+				id: Math.random().toString(),
+				title: data.name,
+				amount: data.price,
+				date: new Date(data.today),
+			},
+		]);
+	};
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
-      return updatedGoals;
-    });
-  };
-
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
-      return updatedGoals;
-    });
-  };
-
-  let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-  );
-
-  if (courseGoals.length > 0) {
-    content = (
-      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
-    );
-  }
-
-  return (
-    <div>
-      <section id="goal-form">
-        <CourseInput onAddGoal={addGoalHandler} />
-      </section>
-      <section id="goals">
-        {content}
-        {/* {courseGoals.length > 0 && (
-          <CourseGoalList
-            items={courseGoals}
-            onDeleteItem={deleteItemHandler}
-          />
-        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-        } */}
-      </section>
-    </div>
-  );
-};
+	return (
+		<>
+			<PaymentForm getPaymentFormData={getPaymentFormData} />
+			<Expenses items={expenses} />
+		</>
+	);
+}
 
 export default App;
