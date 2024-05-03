9a092069511ee068082a5e5aabb4793d73f0b17c
meeyoungchoi <163703903+meeyoungchoi-front-dev@users.noreply.github.com>
2024-05-03 13:14:15 +0900

    firebase에 post요청 보내서 JSON 데이터 저장하기

--- news-list/src/App.js
+++ news-list/src/App.js
@@ -2,8 +2,24 @@ import React from 'react';
 import NewsList from './components/NewsList';
 
 function App() {
+
+  const postRequestHandler = async () => {
+      await fetch('https://react-test-684fe-default-rtdb.firebaseio.com/test.json', {
+        method: 'POST',
+        body: JSON.stringify(
+          {
+            "Key1": "value1",
+            "Key2": "value2"
+          }),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+      })
+  }
+
   return (
-    <NewsList />
+    // <NewsList />
+    <button onClick={postRequestHandler}> onPost</button>
   );
 }
 
